3156944db826f5d226ff35e092df6fa09c86b77a
Turgon37 <pgindraud+github@gmail.com>
2015-04-12 17:33:34 +0200

    Update provided feature
    Delete some unusual features like ip retrieving
    Debug code, update some tools functions for echo, and debug
    Update bash test with new [[ syntax

--- dyn_update.sh
+++ dyn_update.sh
@@ -22,46 +22,37 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-#title			:dyn_update.sh
+#title      :dyn_update.sh
 #description:Use DYN protocol for updating a dynhost with a new ip address
-#author			:P.GINDRAUD
-#author_contact	:pgindraud@gmail.com
-#date			:2014-04-10
-#usage			:./dyn_update.sh
-#usage_info	:
-#options		:NONE
-#notes			:
+#author      :P.GINDRAUD
+#author_contact  :pgindraud@gmail.com
+#date      :2014-04-10
+#usage      :./dyn_update.sh
+#usage_info  :
+#options    :NONE
+#notes      :
 #
-#versions_notes	:
-#	version 2.0 : 2014-06-28
-#				+refund all script syntax the follow the current norm
-#				+adding some documentations strings
+#versions_notes  :
+#  version 2.0 : 2014-06-28
+#        +refund all script syntax the follow the current norm
+#        +adding some documentations strings
 VERSION='2.0'
 #==============================================================================
-# url for ipv4 retrieving
-IPV4_URL="http://bot.whatismyipaddress.com/"
-
 
 
 #========== INTERNAL OPTIONS ==========#
-WGET_PATH="`which wget`"
+readonly WGET_PATH=$(which wget 2>/dev/null)
 
 # static url data for mean that you want to edit a dyn host
 DYN_NIC='/nic/update'
 
-IF_IPV4=1
-
-IPV4_REGEXP='([[:digit:]]{1,3}\.){3}[[:digit:]]{1,3}'
-
-
 
 #========== INTERNAL VARIABLES ==========#
-USER_AGENT="dyn_update/${VERSION}"
-HTTP_GLOBAL_OPTS="--user-agent=${USER_AGENT}"
+USER_AGENT="dyn_update/$VERSION"
+HTTP_GLOBAL_OPTS="--user-agent=$USER_AGENT"
 
 IS_VERBOSE=0
 IS_DEBUG=0
-IS_NO_OUTPUT=0
 
 # remote server to which send dyn update string
 DYNDNS_SERVER='members.dyndns.org'
@@ -77,7 +68,7 @@ DYNDNS_HOSTNAME=''
 # for detail see https://help.dyn.com/remote-access-api/perform-update/
 # Use to identify update type
 #   0 (="dyndns")
-#	1 (="statdns")
+#  1 (="statdns")
 IF_SYSTEM_STATIC=0
 
 # A comma separated list of host to update (max 20)
@@ -115,29 +106,29 @@ URL=
 function _usage() {
   echo -e "Usage : $0 [OPTION] USERNAME PASSWORD DYN_HOSTNAME
 
-  Use DYN protocol for updating a dynhost with a new ip address
+Use DYN protocol for updating a dynhost with a new ip address
 
 Arguments :
-  USERNAME	dyn host to make authentication to server
-  PASSWORD	password associate to the USERNAME
-  DYN_HOSTNAME	dynhost name with the ip to update
+  USERNAME  dyn host to make authentication to server
+  PASSWORD  password associate to the USERNAME
+  DYN_HOSTNAME  dynhost name with the ip to update
+
 Options :
-  -a, --address=IP_ADDRESS	set manually the IP address to use for update
-  the A field instead of the build-in lookup (Recommanded)
-  -d, --destination=IP/HOST	set the dyndns server address
-  that contains the zone to update (Default: '${DYNDNS_SERVER}')
+  -a, --address=IP_ADDRESS  set manually the IP address to use for update the A
+                      field instead of the build-in lookup (Recommanded)
+  -d, --destination=IP/HOST  set the dyndns server address that contains
+                      the zone to update (Default: '${DYNDNS_SERVER}')
   -h, --help     print this help message
-  --no-output    disable all output from wget (if no verbose mode is active)
   -v, --verbose  show more running messages
   -vv, --debug   show debug messages
 
 Dyndns protocol :
-  -b, --backupmx		set backup mx option YES (Default: NOCHG)
-  -!b, --no-backupmx	unset backup mx option NO (Default: NOCHG)
-  -o, --offline		set dyndns to offline mode
-  -s, --static		set static dns option (Default: dynamic)
-  -w, --wildcard 		set wildcard ON (Default: NOCHG)
-  -!w, --no-wildcard 	unset wildcard OFF (Default: NOCHG)"
+  -b, --backupmx      set backup mx option YES (Default: NOCHG)
+  -!b, --no-backupmx  unset backup mx option NO (Default: NOCHG)
+  -o, --offline       set dyndns to offline mode
+  -s, --static        set static dns option (Default: dynamic)
+  -w, --wildcard      set wildcard ON (Default: NOCHG)
+  -!w, --no-wildcard  unset wildcard OFF (Default: NOCHG)"
 }
 
 # Print a msg to stdout if verbose option is set
@@ -179,19 +170,12 @@ function _errorqh() {
   exit 2
 }
 
-
-
-# Get the actual ipv4 address from net
-function _get_ipv4() {
-  ${WGET_PATH} --quiet --output-document=- ${IPV4_URL} | grep --extended-regexp --only-matching --max-count=1 "${IPV4_REGEXP}"
-}
-
 # Build the url option with input parameters
-function _make_query() {
+function _makeQuery() {
   local http_data=
 
   #SYSTEM OPTION
-  if [ ${IF_SYSTEM_STATIC} -eq 1 ]; then
+  if [[ $IF_SYSTEM_STATIC -eq 1 ]]; then
     http_data="${http_data}system=statdns"
   else
     http_data="${http_data}system=dyndns"
@@ -201,25 +185,25 @@ function _make_query() {
   http_data="${http_data}&hostname=${DYNDNS_HOSTNAME}"
 
   # IP DEFINITION
-  if [ ${IF_OFFLINE} -eq 1 ]; then
+  if [[ $IF_OFFLINE -eq 1 ]]; then
     http_data="${http_data}&offline=YES"
   else
     http_data="${http_data}&myip=${MYIP}"
   fi
 
   # OPTION WILDCARD
-  if [ ${IF_WILDCARD} -eq 1 ]; then
+  if [[ $IF_WILDCARD -eq 1 ]]; then
     http_data="${http_data}&wildcard=ON"
-  elif [ ${IF_WILDCARD} -eq -1 ]; then
+  elif [[ $IF_WILDCARD -eq -1 ]]; then
     http_data="${http_data}&wildcard=OFF"
   else
     http_data="${http_data}&wildcard=NOCHG"
   fi
 
   # OPTION BACKMX
-  if [ ${IF_BACKMX} -eq 1 ]; then
+  if [[ $IF_BACKMX -eq 1 ]]; then
     http_data="${http_data}&backmx=YES"
-  elif [ ${IF_WILDCARD} -eq -1 ]; then
+  elif [[ $IF_WILDCARD -eq -1 ]]; then
     http_data="${http_data}&backmx=NO"
   else
     http_data="${http_data}&backmx=NOCHG"
@@ -233,17 +217,17 @@ function _make_query() {
 # Send GET query to server by using the option string give by parameter
 # param[in](string) : http data to include in the query
 function _query() {
-  local opts="--quiet --http-user="${DYNDNS_USERNAME}" --http-password="${DYNDNS_PASSWORD}""
+  local opts="--quiet --http-user=$DYNDNS_USERNAME --http-password=${DYNDNS_PASSWORD}"
   local query="http://${DYNDNS_SERVER}${DYN_NIC}?$1"
   _debug "query = '${query}'"
 
-  #	opts="$opts --post-data $1"
+  #  opts="$opts --post-data $1"
 
-  if [ "${IS_NO_OUTPUT}" -eq 1 ];then
-    ${WGET_PATH} $HTTP_GLOBAL_OPTS $opts --output-document=- "${query}" > /dev/null 2>&1
-  else
-    ${WGET_PATH} $HTTP_GLOBAL_OPTS $opts --output-document=- "${query}"
+  if [[ $IS_VERBOSE -eq 1 ]]; then
+    ${WGET_PATH} $HTTP_GLOBAL_OPTS $opts --output-document=- "$query"
     echo
+  else
+    ${WGET_PATH} $HTTP_GLOBAL_OPTS $opts --output-document=- "$query" > /dev/null 2>&1
   fi
 }
 
@@ -252,8 +236,8 @@ function _query() {
 #========== MAIN FUNCTION ==========#
 
 # Main
-# param	:same of the script
-# return	:
+# param  :same of the script
+# return  :
 function main() {
   ##PARSING ARGUMENTS
   #number of needed arguments
@@ -268,7 +252,6 @@ function main() {
       -d) shift; DYNDNS_SERVER="$1";;
       --destination=*) DYNDNS_SERVER=`echo $1 | cut -d '=' -f 2-`;;
       -h|--help) _usage; exit 0;;
-      --no-output) IS_NO_OUTPUT=1;;
       -o|--offline) IF_OFFLINE=1;;
       -s|--static) IF_SYSTEM_STATIC=1;;
       -v|--verbose) IS_VERBOSE=1;;
@@ -297,14 +280,14 @@ function main() {
 
   # check ip address
   if [[ -z $MYIP ]]; then
-    MYIP="`_get_ipv4`"
+    MYIP="0.0.0.0"
+    _error 'no ip address given, using the default 0.0.0.0'
   fi
 
-  if [[ $IS_NO_OUTPUT -eq 1 ]]; then _debug 'set OPTION no output for wget'; fi
-  if [[ ${IF_OFFLINE} -eq 1 ]]; then _debug 'set OPTION offline'; fi
-  if [[ ${IF_SYSTEM_STATIC} -eq 1 ]]; then _debug 'set OPTION static dns'; fi
-  if [[ ${IF_WILDCARD} -eq 1 ]]; then _debug 'set OPTION wildcard to ON'; fi
-  if [[ ${IF_WILDCARD} -eq -1 ]]; then _debug 'set OPTION wildcard to OFF'; fi
+  if [[ $IF_OFFLINE -eq 1 ]]; then _debug 'set OPTION offline'; fi
+  if [[ $IF_SYSTEM_STATIC -eq 1 ]]; then _debug 'set OPTION static dns'; fi
+  if [[ $IF_WILDCARD -eq 1 ]]; then _debug 'set OPTION wildcard to ON'; fi
+  if [[ $IF_WILDCARD -eq -1 ]]; then _debug 'set OPTION wildcard to OFF'; fi
   _debug "set username to $DYNDNS_USERNAME"
   if [[ $IS_DEBUG -eq 1 ]]; then
     local password_string=
@@ -317,7 +300,7 @@ function main() {
   _debug "set dyn server to ${DYNDNS_SERVER}"
   _debug "using ip address: ${MYIP}"
   
-  _make_query
+  _makeQuery
 }
-        
+
 main "$@"
