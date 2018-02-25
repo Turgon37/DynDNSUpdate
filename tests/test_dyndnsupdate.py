37114e0f43a0d3f61ae68dc08a6086b8eaf8770f
Pierre GINDRAUD <pgindraud@gmail.com>
2018-02-25 19:56:55 +0100

    add tests for http auth

--- tests/test_dyndnsupdate.py
+++ tests/test_dyndnsupdate.py
@@ -24,29 +24,28 @@ def test_without_setting():
 @patch('http.client.HTTPSConnection', createHTTPSConnectionMock())
 def test_with_valid_settings():
     """Must produce an error is bad urls were given"""
-    shutil.rmtree('tmp', ignore_errors=True)
-
     program = dyndnsupdate.DynDNSUpdate()
     assert program.configure(dyndns_myip='1.1.1.1', server_url='www.api.com:81/',
+                                verbose=-1,
                                 dyndns_hostname=['mydyndnshostname.com']) == True
     assert program.main() == 0
 
     program = dyndnsupdate.DynDNSUpdate()
-    assert program.configure(dyndns_myip='1.1.1.1', server_url='https://www.api.com/', dyndns_hostname=['mydyndnshostname.com']) == True
+    assert program.configure(dyndns_myip='1.1.1.1',
+                                verbose=1,
+                                server_url='https://www.api.com/',
+                                dyndns_hostname=['mydyndnshostname.com']) == True
     assert program.main() == 0
 
 
 def test_with_invalid_url():
     """Must produce an error is bad urls were given"""
-    shutil.rmtree('tmp', ignore_errors=True)
-
     program = dyndnsupdate.DynDNSUpdate()
     assert program.configure(server_url='ftp://lmdaz') == False
     assert program.main() == 3
 
 def test_with_missing_settings():
-    """Must produce an error is bad urls were given"""
-    shutil.rmtree('tmp', ignore_errors=True)
+    """Must produce an error is there is any missing setting"""
 
     program = dyndnsupdate.DynDNSUpdate()
     assert program.configure(server_url='http://lmdaz') == True
@@ -54,8 +53,7 @@ def test_with_missing_settings():
 
 @patch('http.client.HTTPConnection', createHTTPConnectionMock())
 def test_with_http_auth():
-    """Must produce an error is bad urls were given"""
-    shutil.rmtree('tmp', ignore_errors=True)
+    """Correct usage of HTTP basic auth"""
 
     program = dyndnsupdate.DynDNSUpdate()
     assert program.configure(dyndns_myip='1.1.1.1',
@@ -66,3 +64,18 @@ def test_with_http_auth():
     c1 = call().request('GET',
     'http://www.api.com/nic/update?system=dyndns&hostname=mydyndnshostname.com&myip=1.1.1.1&wildcard=NOCHG&mx=&backmx=NOCHG&offline=NOCHG&url=', headers={'User-Agent': 'dyndns-update/'+dyndnsupdate.__version__, 'Authorization': 'Basic dXNlcjpwYXNz'})
     http.client.HTTPConnection.assert_has_calls([c1])
+
+@patch('http.client.HTTPSConnection', createHTTPSConnectionMock('0.0.0.0'))
+@patch('ssl._create_unverified_context', return_value=Mock(spec=ssl.SSLContext))
+def test_insecure_https_address_from_url(ssl_context_mock, capsys):
+    """Use insecure SSL transaction"""
+    # https
+    program = dyndnsupdate.DynDNSUpdate()
+    assert program.configure(dyndns_myip='1.1.1.1',
+                            server_url='https://www.api.com/',
+                            tls_insecure=True,
+                            dyndns_hostname=['mydyndnshostname.com'],
+                            server_username='user', server_password='pass') == True
+    assert program.main() == 0
+
+    ssl._create_unverified_context.assert_called_once_with()
