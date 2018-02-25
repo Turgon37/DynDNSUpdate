a86fdbdae831f089e734136f4b158a088d49328c
Pierre GINDRAUD <pgindraud@gmail.com>
2018-02-25 19:39:37 +0100

    add tests

--- tests/test_cmdline.py
+++ tests/test_cmdline.py
@@ -14,3 +14,9 @@ def test_cmdline():
     result = subprocess.Popen(shlex.split('./dyndnsupdate.py --version'), stdout=subprocess.PIPE)
     stdout, stderr = result.communicate()
     assert 'DynDNS client version' in stdout.decode()
+
+def test_cmdline_exit_with_2():
+    """Feed a bad url must produce a 2 return code"""
+    result = subprocess.Popen(shlex.split('./dyndnsupdate.py --dyn-address 1.1.1.1 --dyn-server ftp://www.ovh.com --dyn-hostname d'), stdout=subprocess.PIPE)
+    stdout, stderr = result.communicate()
+    assert result.returncode == 2
--- tests/test_dyndnsupdate.py
+++ tests/test_dyndnsupdate.py
@@ -21,10 +21,32 @@ def test_without_setting():
     assert program.main() == 3
 
 @patch('http.client.HTTPConnection', createHTTPConnectionMock())
+@patch('http.client.HTTPSConnection', createHTTPSConnectionMock())
 def test_with_valid_settings():
     """Must produce an error is bad urls were given"""
     shutil.rmtree('tmp', ignore_errors=True)
 
     program = dyndnsupdate.DynDNSUpdate()
-    program.configure(dyndns_myip='1.1.1.1', server_url='www.api.com/', dyndns_hostname=['mydyndnshostname.com'])
+    assert program.configure(dyndns_myip='1.1.1.1', server_url='www.api.com:81/', dyndns_hostname=['mydyndnshostname.com']) == True
     assert program.main() == 0
+
+    program = dyndnsupdate.DynDNSUpdate()
+    assert program.configure(dyndns_myip='1.1.1.1', server_url='https://www.api.com/', dyndns_hostname=['mydyndnshostname.com']) == True
+    assert program.main() == 0
+
+
+def test_with_invalid_url():
+    """Must produce an error is bad urls were given"""
+    shutil.rmtree('tmp', ignore_errors=True)
+
+    program = dyndnsupdate.DynDNSUpdate()
+    assert program.configure(server_url='ftp://lmdaz') == False
+    assert program.main() == 3
+
+def test_with_missing_settings():
+    """Must produce an error is bad urls were given"""
+    shutil.rmtree('tmp', ignore_errors=True)
+
+    program = dyndnsupdate.DynDNSUpdate()
+    assert program.configure(server_url='http://lmdaz') == True
+    assert program.main() == 3
